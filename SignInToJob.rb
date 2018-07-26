ca812d84da8c76fae48b1f0512ed20a22a57921f
Saim Zahid <saimzahid@me.com>
2018-07-26 12:52:16 -0400

    Swipe action for signature on job sign in.

--- SignInToJob.rb
+++ SignInToJob.rb
@@ -11,8 +11,13 @@ def sign_in_to_job(driver)
     el3 = driver.find_element(:accessibility_id, "Next")
     el3.click
 
-    el4 = driver.find_element(:xpath, "//XCUIElementTypeApplication[@name=\"E-Data Now\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeImage/XCUIElementTypeImage[2]")
+    el4 = driver.find_element(:accessibility_id, "Signature View")
     el4.click
+
+    ::Appium::TouchAction.new.press({x: 238, y: 492}).move_to({x: 914, y: 492}).release.perform
+
+    wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+    wait.until { driver.find_element(:accessibility_id, 'Sign In').enabled? }
 
     el5 = driver.find_element(:accessibility_id, "Sign In")
     el5.click
